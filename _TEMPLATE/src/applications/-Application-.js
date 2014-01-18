64d3fff7e220bf2569ed6d4f37335902302802d2
kodmunki <kodmunki@gmail.com>
2014-01-18 16:56:02 -0600

    Syntax fix

--- _TEMPLATE/src/applications/-Application-.js
+++ _TEMPLATE/src/applications/-Application-.js
@@ -3,5 +3,5 @@ $(function(){
     var appName = "[ENTER YOUR APP NAME HERE]",
         app = $.ku4webApp.app();
     controller = $.ku4webApp.controllers[appName](app)
-    $.ku4webApp.views.[appName](app);
+    $.ku4webApp.views[appName](app);
 });
