047d776a5b344e061f14b7de82a7858a02bb4e62
kodmunki <kodmunki@gmail.com>
2014-01-20 08:56:33 -0600

    Added EOL semicol

--- _TEMPLATE/applications/-Application-.js
+++ _TEMPLATE/applications/-Application-.js
@@ -2,7 +2,7 @@ var controller;
 $(function(){
     var appName = "[ENTER YOUR APP NAME HERE]",
         app = $.ku4webApp.app().throwErrors();
-    controller = $.ku4webApp.controllers[appName](app)
+    controller = $.ku4webApp.controllers[appName](app);
     $.ku4webApp.views[appName](app);
 
 /*======================================================*/
