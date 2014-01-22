a618137debd9685d84538fbf5f6be68b1c231219
kodmunki <kodmunki@gmail.com>
2014-01-22 10:00:56 -0600

    Implement Model and View Singletons

--- src/core/models/model.js
+++ src/core/models/model.js
@@ -7,11 +7,14 @@ $.ku4webApp.model = function(name, proto, subscriptions) {
     $.Class.extend(model, abstractModel);
 
     $.ku4webApp.models[name] = function(mediator, serviceFactory, storeFactory, validatorFactory) {
-        var _model = new model(mediator, serviceFactory, storeFactory, validatorFactory);
-        if($.exists(subscriptions))
-            $.hash(subscriptions).each(function(obj) {
-                mediator.subscribe(obj.key, _model[obj.value], _model);
-            });
-        return _model;
+        if(!$.exists(this.__instance)) {
+            var _model = new model(mediator, serviceFactory, storeFactory, validatorFactory);
+            if($.exists(subscriptions))
+                $.hash(subscriptions).each(function(obj) {
+                    mediator.subscribe(obj.key, _model[obj.value], _model);
+                });
+            this.__instance = _model;
+        }
+        return this.__instance;
     }
 }
--- src/core/views/view.js
+++ src/core/views/view.js
@@ -10,11 +10,15 @@ $.ku4webApp.view = function(name, proto, subscriptions) {
         var className = $.str.format("$.ku4webApp.views.{0}", name),
             message = $.str.format("Requires a valid app. app= {0}", app);
         if(!$.exists(app)) throw $.ku4exception(className, message);
-        var _view = new view(app.templateFactory, app.formFactory);
-        if($.exists(subscriptions))
-            $.hash(subscriptions).each(function(obj) {
-                app.mediator.subscribe(obj.key, _view[obj.value], _view);
-            });
-        return _view;
+
+        if(!$.exists(this.__instance)) {
+            var _view = new view(app.templateFactory, app.formFactory);
+            if($.exists(subscriptions))
+                $.hash(subscriptions).each(function(obj) {
+                    app.mediator.subscribe(obj.key, _view[obj.value], _view);
+                });
+            this.__instance = _view;
+        }
+        return this.__instance;
     }
 }
