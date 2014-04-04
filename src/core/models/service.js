c454aaa5082325524b24c85bceccf8bdee2ae1c1
kodmunki <kodmunki@gmail.com>
2014-04-03 23:13:36 -0500

    Add latest dependencies

--- src/core/models/service.js
+++ src/core/models/service.js
@@ -1,13 +1,16 @@
 function service(mediator, config) {
     this._mediator = mediator;
     this._config = config;
+    this._noCache = false;
 }
 service.prototype = {
+    noCache: function() { this._noCache = true; return this; },
     call: function(params) {
         var config = this._config,
             mediator = this._mediator,
             service = $.service()[config.verb]().uri(config.uri);
         service.contentType(config.contentType);
+        if(this._noCache) service.noCache();
         service.onSuccess(function(data) {
                 if($.exists(config.success))
                     mediator.notify(data, service.processId(), config.success);
