be0fa8b9cb753160e3be718df52fdba19f5c74e5
kodmunki <kodmunki@gmail.com>
2014-04-19 12:22:24 -0500

    Update the update script and structure

--- _build/update.sh
+++ _build/update.sh
@@ -2,8 +2,7 @@ sh build.sh
 sh build_testBundle.sh
 
 echo "Copying resources to _TEMPLATE/"
-cp -f ../bin/ku4jQuery-webApp.js ../_TEMPLATE/lib
-cp -f ../bin/ku4jQuery-webApp-testBundle.js ../_TEMPLATE/lib
+cp -f ../bin/ku4jQuery-webApp-testBundle.js ../_TEMPLATE/tests/_dependencies/
 
 echo "Copying resources to example/"
 cp -f ../bin/ku4jQuery-webApp.js ../example/scripts/example/lib/
