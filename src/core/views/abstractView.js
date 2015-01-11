245e8edff9be168248e6704479985253e0b7efda
kodmunki <kodmunki@gmail.com>
2015-01-11 07:14:40 -0600

    Add view states

--- src/core/views/abstractView.js
+++ src/core/views/abstractView.js
@@ -2,10 +2,12 @@ function abstractView(templateFactory, formFactory, navigator) {
     this._templateFactory = classRefcheck("views", "templateFactory", templateFactory);
     this._formFactory = classRefcheck("views", "formFactory", formFactory);
     this._navigator = classRefcheck("models", "navigator", navigator);
+    this._state = new state();
 }
 abstractView.prototype = {
     $template: function(name) { return this._templateFactory.create(name); },
     $form: function(name) { return this._formFactory.create(name); },
-    $navigator: function() { return this._navigator; }
+    $navigator: function() { return this._navigator; },
+    $state: function() { return this._state; }
 };
 $.ku4webApp.abstractView = abstractView;
