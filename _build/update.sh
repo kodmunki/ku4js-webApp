d41aaf405f57e1560c005359c96e297dc0073731
kodmunki <kodmunki@gmail.com>
2016-10-02 09:25:49 -0500

    Update webapp

--- _build/update.sh
+++ _build/update.sh
@@ -8,7 +8,7 @@ echo "Copying resources to example/"
 cp -f ../bin/ku4js-webApp.js ../_example/javascript/lib/
 cp -f ../bin/ku4js-webApp-testBundle.js ../_example/javascript/tests/_dependencies/
 
-echo "Copying resources to _LATEST/"
+echo "Copying resources to _latest/"
 cp -f ../bin/ku4js-webApp.js ../../_latest/
 cp -f ../bin/ku4js-webApp-testBundle.js ../../_latest/
 
